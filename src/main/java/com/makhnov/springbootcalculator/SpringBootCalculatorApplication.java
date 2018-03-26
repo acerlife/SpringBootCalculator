c27ed6dd05125ea2118fa14ffe85412ec6fe3de3
Yaroslav Makhnov <makhnovyar@ukr.net>
2018-03-26 15:46:32 +0300

    SpringBootCalculator

---
+++ src/main/java/com/makhnov/springbootcalculator/SpringBootCalculatorApplication.java
@@ -0,0 +1,12 @@
+package com.makhnov.springbootcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCalculatorApplication.class, args);
+    }
+}
