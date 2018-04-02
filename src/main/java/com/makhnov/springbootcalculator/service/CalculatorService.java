b1ed1b07cfb921164c404aa1f2192dd520596e8a
Yaroslav Makhnov <makhnovyar@ukr.net>
2018-04-02 14:18:58 +0300

    use CTOR Injection

--- src/main/java/com/makhnov/springbootcalculator/service/CalculatorService.java
+++ src/main/java/com/makhnov/springbootcalculator/service/CalculatorService.java
@@ -1,15 +1,24 @@
 package com.makhnov.springbootcalculator.service;
 
 import com.makhnov.springbootcalculator.dao.CalculatorDao;
+import org.jooq.Field;
+import org.jooq.Record3;
+import org.jooq.SelectJoinStep;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class CalculatorService {
-    @Autowired
-    private CalculatorDao calculatorDao;
+    private final CalculatorDao calculatorDao;
 
-    public void saveResult(String postfix_expression) throws Exception{
+    @Autowired
+    public CalculatorService(CalculatorDao calculatorDao){
+        this.calculatorDao = calculatorDao;
+    }
+
+    public void saveResult(String postfix_expression) throws Exception {
         calculatorDao.saveResult(postfix_expression);
     }
 }
