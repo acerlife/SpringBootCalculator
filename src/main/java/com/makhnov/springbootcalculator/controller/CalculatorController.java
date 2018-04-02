53138b2017b30b819c4366381880cbf6c7acc6d7
Yaroslav Makhnov <makhnovyar@ukr.net>
2018-04-02 14:17:36 +0300

    add return into saveResult

--- src/main/java/com/makhnov/springbootcalculator/controller/CalculatorController.java
+++ src/main/java/com/makhnov/springbootcalculator/controller/CalculatorController.java
@@ -1,18 +1,24 @@
 package com.makhnov.springbootcalculator.controller;
 
-import com.makhnov.springbootcalculator.dao.CalculatorDao;
 import com.makhnov.springbootcalculator.service.CalculatorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
 public class CalculatorController {
+    private final CalculatorService calculatorService;
+
     @Autowired
-   private CalculatorService calculatorService;
+    public CalculatorController(CalculatorService calculatorService){
+        this.calculatorService = calculatorService;
+    };
 
     @GetMapping("/{postfix_expression}")
-    public @ResponseBody void saveResult(@PathVariable String postfix_expression) throws Exception{
-        calculatorService.saveResult(postfix_expression);
+    public @ResponseBody
+    HttpStatus saveResult(@PathVariable String postfix_expression) throws Exception{
+       calculatorService.saveResult(postfix_expression);
+       return HttpStatus.OK;
     }
 }
