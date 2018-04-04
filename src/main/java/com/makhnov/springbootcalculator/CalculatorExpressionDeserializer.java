b3dd483f141fa3693a24d983b0db22b4e94e4314
Yaroslav Makhnov <makhnovyar@ukr.net>
2018-04-04 14:41:18 +0300

    add CalculatorExpression class and deserializer

---
+++ src/main/java/com/makhnov/springbootcalculator/CalculatorExpressionDeserializer.java
@@ -0,0 +1,18 @@
+package com.makhnov.springbootcalculator;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.io.IOException;
+
+public class CalculatorExpressionDeserializer extends JsonDeserializer {
+    @Override
+    public Object deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+        JsonNode node = jsonParser.getCodec().readTree(jsonParser);
+        String postfixExpression = node.get("postfixExpression").textValue();
+        double expectedResult = node.get("expectedResult").doubleValue();
+        return new CalculatorExpression(postfixExpression, expectedResult);
+    }
+}
